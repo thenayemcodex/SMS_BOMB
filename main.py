fb6a205461f5087035222010450d0ec2c9c9cfb9
Noob-Hacker71 <80197475+Noob-Hacker71@users.noreply.github.com>
2021-11-29 22:46:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# Do Not Try To Copy This Code
+# If You Can Decode This Code ... Tool Is Your
+# Mao2116 & Noob-Hacker71
+# We are Together forever 
 import sys
-
+from time import sleep
 if sys.version[0:4]=="3.10":
   import noob_hacker71
 else:
+  time.sleep(1)
   print("\033[1;31mUse Python Version 3.10")
+  time.sleep(2)
   exit()
